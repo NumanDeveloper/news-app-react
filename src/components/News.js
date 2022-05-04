a3c874adddc098c3b493f1bbe8b62d855c8229a5
Numan <numaniftikhar300@gmail.com>
2022-05-04 20:23:14 +0500

    fetched data using fetch api

--- src/components/News.js
+++ src/components/News.js
@@ -2,19 +2,77 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+    handleNextClick = async () => {
+        console.log("next click")
+        // check if next page exits or not 
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 16)) {
+            //pass
+        }
+        else {
+            // News API's URL
+            let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=f3ae35dedebf4e3f9341ae2ab46c878b&page=${this.state.page + 1}&pageSize=16`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
+    }
+    handlePrevClick = async () => {
+        console.log("prev click")
+        // News API's URL
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=f3ae35dedebf4e3f9341ae2ab46c878b&page=${this.state.page - 1}&pageSize=16`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles,
+        });
+    }
+    constructor() {
+        super();
+        this.state = {
+            articles: [],
+            loading: false,
+            page: 1,
+        }
+    }
+
+    async componentDidMount() {
+        // Called immediately after a component is mounted/rendered.
+        console.log("cdm")
+        // News API's URL
+        let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=f3ae35dedebf4e3f9341ae2ab46c878b&page=1&pageSize=16";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults,
+        });
+    }
+
     render() {
         return (
-            <div className='container my-4 d-flex justify-content-around'>
-                <div className="row">
-                    <div className="col-md-4">
-                        <NewsItem title="myTitle" description="myDesc" />
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="myTitle" description="myDesc" />
-                    </div>
-                    <div className="col-md-4">
-                        <NewsItem title="myTitle" description="myDesc" />
-                    </div>
+            <div className='container'>
+                {/* Previous and Next button section */}
+                <div className="container my-3 d-flex justify-content-between">
+                    <button type="button" className="btn btn-primary" disabled={this.state.page <= 1} onClick={this.handlePrevClick}>&laquo; Previous</button>
+                    <button type="button" className="btn btn-primary" onClick={this.handleNextClick}>Next &raquo;</button>
+                </div>
+                <div className="row my-4 d-flex justify-content-start">
+                    <h1>News Monkey - Latest News</h1>
+                    {this.state.articles.map((element, key) => {
+                        // console.log(element);
+                        return <div className="col-md-3" key={key}>
+                            <NewsItem title={element.title ? element.title.slice(0, 40) : ""} description={element.description ? element.description.slice(0, 55) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                        </div>
+                    })}
+                </div>
+                {/* Previous and Next button section */}
+                <div className="container my-3 d-flex justify-content-between">
+                    <button type="button" className="btn btn-primary" disabled={this.state.page <= 1} onClick={this.handlePrevClick}>&laquo; Previous</button>
+                    <button type="button" className="btn btn-primary" onClick={this.handleNextClick}>Next &raquo;</button>
                 </div>
             </div>
         )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let { title, description } = this.props; // JavaScript Destructuring
+        let { title, description, imageUrl, newsUrl } = this.props; // JavaScript Destructuring
         return (
-            <div>
+            <div className='my-3'>
                 <div className="card" style={{ width: '18rem' }}>
-                    <img src="https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png" className="card-img-top" alt="..." />
+                    <img src={imageUrl?imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQsq1NacYKHKS-RudSBgbLZa_ndkD-lmmQfA&usqp=CAU"} alt="news img" style={{ height: '13pc' }} />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href="/newsItem" className="btn btn-primary">Go somewhere</a>
+                        <h5 className="card-title">{(title.length < 40 ? title : title + "...")}</h5>
+                        <p className="card-text">{(description.length < 55 ? description : description + "...")}...</p>
+                        <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
